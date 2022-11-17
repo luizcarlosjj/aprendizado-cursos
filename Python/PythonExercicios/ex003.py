9dd27e0e40e91c6eb0e9a51b2308e0c9ef41a9ab
Luiz Carlos <dia.a.diajj@gmail.com>
2022-11-17 16:52:41 -0300

    upload files

---
+++ Python/PythonExercicios/ex003.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um valor: ')) #Números inteiros(1,5,10,1000)
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+print('O Valor da soma entre {} e {} é: {}' .format(n1, n2, s))
---
+++ Python/PythonExercicios/ex004.py
@@ -0,0 +1,9 @@
+a = input('Digite um valor: ')
+print('O tipo primitivo desse valor é ', type(a))
+print('Só tem espaços?', a.isspace())
+print('é um número?', a.isnumeric())
+print('É alfabetico?', a.isalpha())
+print('É alfanumerico?', a.isalnum())
+print('Esta em Maisculas', a.isupper())
+print('Esta em Minusculas', a.islower())
+
