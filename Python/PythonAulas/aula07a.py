f22bc5184c0ae139b41418c7920f55a0f9ccc728
Luiz Carlos <dia.a.diajj@gmail.com>
2022-11-30 19:41:28 -0300

    update studing

---
+++ Python/PythonAulas/aula07a.py
@@ -0,0 +1,12 @@
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print(f'A some é {s}, a multiplicação é {m} \n e a divisão é {d}', end=' ')
+print(f'Divisão inteira é {di} e potência {e}')
+
+# \n == quebra de linha
+# end= '' == continuação de linha
